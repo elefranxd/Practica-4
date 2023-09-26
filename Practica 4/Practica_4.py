409a0bf708656e03fc65b2d6c26c410c95bb906d
elefranxd <efrainmartinez2802@outlook.com>
2023-09-26 12:29:38 -0600

    FInal

--- Practica 4/Practica_4.py
+++ Practica 4/Practica_4.py
@@ -1,6 +1,19 @@
 import tkinter as tk
-
-
+def Convertir_a_celsius():
+    fahrenheit = float (entry1.get())
+    celsius = (fahrenheit-32)*5.0/9.0
+    entry2.delete(0, tk.END)
+    entry2.insert(0, f"{celsius:.2F}")
+def Convertir_a_fahrenheit():
+    Celsius = float (entry2.get())
+    fahrenheit = (celsius*9/5)+32
+    entry1.delete(0, tk.END)
+    entry1.insert(o, f"{Fahrenheit:.2F}")
+def borrar():
+    entry1.delete(0, tk.END)
+    entry1.insert(0, "0.0")
+    entry2.delete(0, tl.END)
+    entry2.insert(0, "0.0")
 app = tk.Tk()
 app.title("Conversoe de Temperatura")
 
@@ -10,7 +23,7 @@ label1.grid(row = 0, column = 0)
 entry1 = tk.Entry(app)
 entry1.grid(row = 0, column = 1)
 
-button1 = tk.Button(app, text = "Convertir a Celsius")#, command = convertir_a_celsius)
+button1 = tk.Button(app, text = "Convertir a Celsius", command = Convertir_a_celsius)
 button1.grid(row = 0, column = 2)
 
 label2 = tk.Label(app, text = "Celsius:")
@@ -19,10 +32,10 @@ label2.grid(row = 1, column = 0)
 entry2 = tk.Entry(app)
 entry2.grid(row = 1, column = 1)
 
-button2 = tk.Button(app, text = "Convertir a Fahrenheit")#, command = convertir_a_Fahrenheit)
+button2 = tk.Button(app, text = "Convertir a Fahrenheit", command = Convertir_a_fahrenheit)
 button2.grid(row = 1, column = 2)
 
-button3 = tk.Button(app, text = "Restablecer")#, command = borrar)
+button3 = tk.Button(app, text = "Restablecer", command = borrar)
 button3.grid(row = 2, column = 1)
 
 app.mainloop()
